4cda32ef630c6351755b832cb0b1bd2a8bb90374
Maido Käära <maido@producement.com>
2022-12-09 12:37:38 +0200

    Upload artifacts

--- app/jni/src/Android.mk
+++ app/jni/src/Android.mk
@@ -8,6 +8,8 @@ LOCAL_C_INCLUDES := $(LOCAL_PATH)/../m8c
 
 LOCAL_SRC_FILES := $(wildcard $(LOCAL_PATH)/*.c)
 
+LOCAL_CFLAGS += -DUSE_LIBUSB
+
 LOCAL_SHARED_LIBRARIES := m8c
 
 LOCAL_LDLIBS := -lGLESv1_CM -lGLESv2 -lOpenSLES -llog -landroid
