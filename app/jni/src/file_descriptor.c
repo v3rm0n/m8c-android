14e9e301c8fe9095b60a2c2f72bed0f922b6d320
Maido Käära <maido@producement.com>
2022-12-12 16:38:07 +0200

    Refactor audio

--- app/jni/src/file_descriptor.c
+++
@@ -1,8 +0,0 @@
-#include <jni.h>
-#include <serial.h>
-
-JNIEXPORT void JNICALL
-Java_io_maido_m8client_M8SDLActivity_setFileDescriptor(JNIEnv *env, jobject thiz,
-                                                       jint fd) {
-    set_file_descriptor(fd);
-}
