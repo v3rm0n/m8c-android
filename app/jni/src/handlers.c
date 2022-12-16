41180925ad72d9c3b56fd5eb72cfa0cf4aeacb0b
Maido Käära <maido@producement.com>
2022-12-16 13:59:31 +0200

    Default to aaudio when available

--- app/jni/src/handlers.c
+++ app/jni/src/handlers.c
@@ -2,6 +2,7 @@
 #include <serial.h>
 #include "audio.h"
 #include <SDL.h>
+#include <android/api-level.h>
 
 JNIEXPORT void JNICALL
 Java_io_maido_m8client_M8SDLActivity_loop(JNIEnv *env, jobject thiz) {
@@ -11,6 +12,9 @@ Java_io_maido_m8client_M8SDLActivity_loop(JNIEnv *env, jobject thiz) {
 JNIEXPORT void JNICALL
 Java_io_maido_m8client_M8SDLActivity_connect(JNIEnv *env, jobject thiz,
                                              jint fd, jint audiodevice) {
+    if (android_get_device_api_level() > 27) {
+        SDL_SetHint(SDL_HINT_AUDIODRIVER, "aaudio");
+    }
     set_audio_device(audiodevice);
     set_usb_init_callback(audio_setup);
     set_usb_destroy_callback(audio_destroy);
